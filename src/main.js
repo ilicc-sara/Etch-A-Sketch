39edb8a25a35f414cbc1c2fee5847261923b8f65
Sara <sarailic431@gmail.com>
2024-12-21 20:10:35 +0100

    adding color

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
 "use strict";
 import "./style.css";
 
-const inputColor = document.querySelector(".choose-color");
+const inputColorEl = document.querySelector(".choose-color");
 
-inputColor.addEventListener("input", function () {
-  console.log(inputColor.value);
+const boardContainer = document.querySelector(".board-container");
+
+inputColorEl.addEventListener("input", function () {
+  console.log(inputColorEl.value);
 });
 
 const buttons = document.querySelectorAll(".btn");
 console.log(buttons);
+
+const sketcher = function () {
+  let color = "red";
+
+  const colorMode = () => (color = inputColorEl.value);
+  return { colorMode };
+};
+
+const sketch = sketcher();
+
+boardContainer.addEventListener("mouseover", function (e) {
+  console.log((e.target.style.backgroundColor = sketch.colorMode()));
+});
