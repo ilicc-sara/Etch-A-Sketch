6bc36822e9d6b9ab25ec5a600b98bc5f376aa86b
Sara <sarailic431@gmail.com>
2024-12-21 20:19:58 +0100

    adding different modes

--- src/main.js
+++ src/main.js
@@ -9,6 +9,12 @@ inputColorEl.addEventListener("input", function () {
   console.log(inputColorEl.value);
 });
 
+const randomNum = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomNum(0, 225)}, ${randomNum(0, 255)}, ${randomNum(0, 255)})`;
+
 const buttons = document.querySelectorAll(".btn");
 console.log(buttons);
 
@@ -16,11 +22,18 @@ const sketcher = function () {
   let color = "red";
 
   const colorMode = () => (color = inputColorEl.value);
-  return { colorMode };
+
+  const randomMode = () => (color = randomColor());
+
+  const eraser = () => (color = "white");
+
+  return { colorMode, randomMode, eraser };
 };
 
 const sketch = sketcher();
 
 boardContainer.addEventListener("mouseover", function (e) {
-  console.log((e.target.style.backgroundColor = sketch.colorMode()));
+  // console.log((e.target.style.backgroundColor = sketch.colorMode()));
+  // console.log((e.target.style.backgroundColor = sketch.randomMode()));
+  // console.log((e.target.style.backgroundColor = sketch.eraser()));
 });
